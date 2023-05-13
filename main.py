5766b2f2b65e5067ddad27447a2955aa457bbf24
Bananasmoothii <45853225+bananasmoothii@users.noreply.github.com>
2023-05-13 17:47:32 +0200

    renamed some vars

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from pygame.locals import *
 
 import Enemy
 import Player
-from game_over import game_over
+import game_over
 
 # Predefined some colors
 BLUE = (0, 0, 255)
@@ -24,25 +24,18 @@ BACKGROUND_SPEED = 5
 
 FPS = 60
 
-DISPLAYSURF = None
-
-player = None
-
-ennemies = []
 
 def main():
-    global DISPLAYSURF, player, ennemies
-
     pygame.init()
-    FramePerSec = pygame.time.Clock()
+    clock = pygame.time.Clock()
 
-    DISPLAYSURF = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    DISPLAYSURF.fill(WHITE)
+    display_surf = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    display_surf.fill(WHITE)
     pygame.display.set_caption("Game")
 
     player = Player.Player()
 
-    ennemies = [Enemy.Enemy() for i in range(10)]
+    ennemies = [Enemy.Enemy() for _ in range(10)]
     grass = pygame.image.load("images/grass.jpg")
 
     bg_offset = 0
@@ -59,27 +52,26 @@ def main():
         bg_offset = bg_offset % 100
         for x in range(0, SCREEN_WIDTH, 100):
             for y in range(-100, SCREEN_HEIGHT, 100):
-                DISPLAYSURF.blit(pygame.transform.scale(grass, (100, 100)), (x, y + bg_offset))
+                display_surf.blit(pygame.transform.scale(grass, (100, 100)), (x, y + bg_offset))
 
         # Draw ennemies
         player_coords = player.rect.center
         player_radius = player.rect.height / 2
         for ennemie in ennemies:
             ennemie.move()
-            ennemie.draw(DISPLAYSURF)
+            ennemie.draw(display_surf)
 
             # collision detection
             ennemie_coords = ennemie.rect.center
             ennemie_radius = ennemie.rect.height / 2
             if math.sqrt((player_coords[0] - ennemie_coords[0]) ** 2 + (player_coords[1] - ennemie_coords[1]) ** 2) < (
                     player_radius + ennemie_radius):
-                game_over()
+                game_over.game_over()
 
-        player.draw(DISPLAYSURF)
+        player.draw(display_surf)
 
         pygame.display.update()
-        FramePerSec.tick(FPS)
-
+        clock.tick(FPS)
 
 
 if __name__ == "__main__":
